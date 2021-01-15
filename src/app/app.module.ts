a7abce4556e720bb1f398afc0c4384356ef3a80f
dzhi-ya <66246401+dzhi-ya@users.noreply.github.com>
2021-01-15 21:24:14 +0300

    Update app.module.ts
    upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,6 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-// import { AppComponent } from './app.component';
-// import { TableWorkersComponent } from './ui/table-workers/table-workers.component';
-// import { AddformWorkerComponent } from './ui/addform-worker/addform-worker.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     TableWorkersComponent,
-//     AddformWorkerComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { TableWorkersComponent } from './ui/table-workers/table-workers.component';
